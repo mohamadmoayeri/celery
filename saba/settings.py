174992a62dfc299d0021733ef8d611de47926d13
mhmd <mhmd@yahoo.com>
2021-02-15 20:56:09 +0330

    celery Beat

--- saba/settings.py
+++ saba/settings.py
@@ -135,6 +135,6 @@ CELERY_RESULT_BACKEND='rpc://'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'mohammad.moayyeri10@gmail.com'
-EMAIL_HOST_PASSWORD = 'xxkvvrdsdulsvhmf'
+EMAIL_HOST_PASSWORD = 'private'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
